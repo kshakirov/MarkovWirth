5ee02ec6c570a402afc4b2b58e708d00dc455658
Kirill Shakirov <kirill.shakirov4@gmail.com>
2026-07-02 15:14:41 +0300

    ks-1 v parser prototype ready

--- bin/parse_csv.js
+++ bin/parse_csv.js
@@ -3,87 +3,101 @@ import fs from 'node:fs'
 console.log("creating the parser")
 
 
-const csv = `
-pos,id,name
-1,100,Kirill
-2,200,Vasiliy
+const csv = `pos,id,name
+1,100,k
 `
 
+function parseUnescaped(str, acc){
+  //  console.log(str)
 
-
-
-
-
-function markEscaped(){
-    current_stage = "ESCAPED";
-}
-
-function markUnescaped(){
-}
-
-function markEscapedBody(){
-}
-
-function markEmpty(){
-    
-}
-
-function markLbrk(){
-    
-}
-
-function runEscped(){
-    
-}
-
-
-
-function parseUnescaped(str){
     if(str[0]!= "," && str[0]!="\n"){
-	parseUnescaped(str.slice(1))
-	return str[0];
-    }else{
-	parseFile(str.slice(1));
-    }
-}
-      
-function parseEscaped(str){
-    //check if not another "" later
-    if(str[0]==","){
-	parseEscaped(str.slice(1));
-	return ch;
-    }else{
-	parseFile()
-    }
-}
+	return parseUnescaped(str.slice(1), acc + str[0]);
 
-
-function  parseFile(str){
-    let row = []
-//    console.log(`parseFile ${str}`)
-    if(str.length == 0){
-	console.log("Finished");
-	return row;
+    }else if(str[0]=="\n" || str[0]==","){
 	
-    }
-    else if(str[0]!=="\""){
-	let cell = [];
-	cell.push(parseUnescaped(str.slice(1)));
-	//console.log(cell);
-	row.push(cell);
+	return [acc, str];
 
-    }else if(str[0]=="\"" && str[0] != "\n"){
-	 parseEscaped(str.slice(1))
-    }else if(str[0]=="\n" && str.length > 1){
-	 return parseFile(str.slice(1));
+    }
+    
+}
+
+// function parseEscaped(str, options){
+//     console.log(str[0])
+//     if(str[0]!=="\"" && str[0]!="\n"){
+
+// 	return str[0] + parseEscaped(str.slice(1));
+
+//     }else if(str[0]=="\""){
+
+// 	return str[0] + parseEscaped(str.slice(1), {quote_flag: true, occurence: 1});;
+
+//     }
+//     else if (options && options.quote_flag){		
+// 	if(str[0]=="\n" && options.occurence == 1 ){
+// 	    parseFile(str.slice(1));
+// 	    return "__NEWLINE___";
+	    
+// 	}else if(str[0]=="\"" && options.occurence == 1){
+// 	    return str[0] + parseEscaped(str.slice(1), {quote_flag: true, occurence: 2});
+// 	}else if(str[0]=="\"" && options.occurence == 2){
+// 	    return str[0] + parseEscaped(str.slice(1));
+// 	}
+	
+//     }
+//     else{
+// 	parseFile(str.slice(1));
+// 	return ""
+
+//     }
+// }
+
+function addCellToArray(cell, array){
+    array[0].push(cell);
+    return array
+}
+
+function addEmptyArrayToNewLine(func){
+    
+    return [[], ...func];
+}
+
+function parseFile(str){
+    
+    if(str.length > 0){
+	if(str[0]!=="\"" && str[0]!="\n" && str[0] !=","){
+	    let par = str;
+	    let [cell , n_str] = parseUnescaped(par, "");
+//	    return [cell, ...parseFile(n_str)]
+	    return addCellToArray(cell, parseFile(n_str))
+	}else if(str[0]=="\"" && str[0] != "\n"){
+	    let cell, str = parseEscaped(str.slice(1), {quote_flag: false})
+	}else if (str[0] == ","){
+	    //	    console.log(`It it the end of a cell ${str}`);
+	    return  parseFile(str.slice(1)) ;
+
+	    
+	}else if(str[0]=="\n"){
+	    console.log(str.length)
+	    if(str.length >= 1){
+		return  addEmptyArrayToNewLine(parseFile(str.slice(1))) ;
+//		return  parseFile(str.slice(1)) ;
+	    }else{
+		return parseFile([])
+	    }
+
+	}
     }
     else {
 	
-	console.log("some unkonwn character " + ch );
+	console.log("Finishing ");
+	return [] ;
     }
 
 }
 
+
+
 let result = parseFile(csv);
 
-console.log(result);
+
+console.log(JSON.stringify(result));
