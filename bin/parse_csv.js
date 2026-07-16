5b629b015e4989937e53f7f9148e462a819706c9
kshakirov <kirill.shakirov4@gmai.com>
2026-07-16 14:51:19 +0300

    rebased

--- bin/parse_csv.js
+++ bin/parse_csv.js
@@ -1,58 +1,39 @@
-import fs from 'node:fs'
-
+import {MarkovEngine} from "./../lib/parser/markov/parse_escaped.js"
 console.log("creating the parser")
 
 
-const csv = `pos,id,name
-1,100,k
+const csv = `pos,id,name,
+1," here it is 100 ",a,
+2,200,k,
+3," tell me """ here """ ","kkk ee",
+4,400,a
 `
 
 function parseUnescaped(str, acc){
-  //  console.log(str)
-
     if(str[0]!= "," && str[0]!="\n"){
 	return parseUnescaped(str.slice(1), acc + str[0]);
-
     }else if(str[0]=="\n" || str[0]==","){
-	
 	return [acc, str];
-
     }
-    
 }
 
-// function parseEscaped(str, options){
-//     console.log(str[0])
-//     if(str[0]!=="\"" && str[0]!="\n"){
+function parseEscaped(str, acc){
+    console.log(`Entering parseescaped with acc ${acc}`);
+    let [result, orig] = MarkovEngine(acc);
+    if(result){
+	let ro = orig.slice(0,-1);
+	console.log(`All parsed: ${orig} without , ${ro} `);
+	return [ro,str]
+    }else if (result == null){
+	console.log(`Not Yet Parsed: ${acc}`);
+	return parseEscaped(str.slice(1), acc + str[0]);
+    }
 
-// 	return str[0] + parseEscaped(str.slice(1));
+}
 
-//     }else if(str[0]=="\""){
-
-// 	return str[0] + parseEscaped(str.slice(1), {quote_flag: true, occurence: 1});;
-
-//     }
-//     else if (options && options.quote_flag){		
-// 	if(str[0]=="\n" && options.occurence == 1 ){
-// 	    parseFile(str.slice(1));
-// 	    return "__NEWLINE___";
-	    
-// 	}else if(str[0]=="\"" && options.occurence == 1){
-// 	    return str[0] + parseEscaped(str.slice(1), {quote_flag: true, occurence: 2});
-// 	}else if(str[0]=="\"" && options.occurence == 2){
-// 	    return str[0] + parseEscaped(str.slice(1));
-// 	}
-	
-//     }
-//     else{
-// 	parseFile(str.slice(1));
-// 	return ""
-
-//     }
-// }
 
 function addCellToArray(cell, array){
-    array[0].push(cell);
+    array[0].unshift(cell);
     return array
 }
 
@@ -67,12 +48,12 @@ function parseFile(str){
 	if(str[0]!=="\"" && str[0]!="\n" && str[0] !=","){
 	    let par = str;
 	    let [cell , n_str] = parseUnescaped(par, "");
-//	    return [cell, ...parseFile(n_str)]
 	    return addCellToArray(cell, parseFile(n_str))
 	}else if(str[0]=="\"" && str[0] != "\n"){
-	    let cell, str = parseEscaped(str.slice(1), {quote_flag: false})
+	    let par = str;
+	    let [cell, n_str] = parseEscaped(par,"")
+	    return addCellToArray(cell, parseFile(n_str))
 	}else if (str[0] == ","){
-	    //	    console.log(`It it the end of a cell ${str}`);
 	    return  parseFile(str.slice(1)) ;
 
 	    
@@ -100,4 +81,7 @@ function parseFile(str){
 let result = parseFile(csv);
 
 
-console.log(JSON.stringify(result));
+//console.log(JSON.stringify(result));
+for (let r of result){
+    console.log(r);
+}
