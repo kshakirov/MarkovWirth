a31d57112109b75914cd955716f1e31eada98bf3
Kirill Shakirov <kirill.shakirov4@gmail.com>
2026-08-05 12:54:39 +0300

    testing: wip 3

--- test/test_positive.js
+++ test/test_positive.js
@@ -17,19 +17,49 @@ function generatePositiveBufferWithOnlyEnd(length){
 function verifyPositiveWithOnlyEnd(n){
     const startIndex=0;
     for(let i = 0; i< n; i++){
-	const testLength = Math.floor(Math.random() * 200) + 2; // длины от 1 до 200 байт
+	const testLength = Math.floor(Math.random() * 200) + 3; // длины от 1 до 200 байт
+	//const testLength = 32;
 	let buf =generatePositiveBufferWithOnlyEnd(testLength);
-	//console.log(buf.length)
+	//console.log(buf)
 	let [result, index, n_buff] = MarkovEngine(startIndex, buf);
 	//console.log(buf);
 	//console.log(result);
 	if(!result){
 	    console.log(`The length is ${buf.length}`)
 	    console.log("Failed the index is below");
-	    console.log(buf[index], `${buf[index]}`);
-	    console.log(`buf is ${buf}`);
+	    console.log(index, `${buf[index]}`);
+	    console.log(buf);
 	}
     }
 }
 
-verifyPositiveWithOnlyEnd(10);
+verifyPositiveWithOnlyEnd(100);
+
+
+const evilBuffer = Buffer.from([
+    0x53, 0xb6, 0x3a, 0xed, 0xe8, 0x8c, 0xf0, 0x40, 
+    0xa2, 0xfb, 0x8b, 0x46, 0x85, 0xe2, 0xbc, 0x32, 
+    0xd5, 0x80, 0xb1, 0x3c, 0xc7, 0x94, 0x22, 0x2C
+]);
+
+const evilBuffer17 = Buffer.from([
+    0xd0, 0x60, 0x4d, 0xa6, 0xf0, 0xfe, 0xc4, 0xa4, 
+    0xca, 0xe2, 0x98, 0x24, 0x4a, 0xb8, 0x29, 0x22, 0x2c
+]);
+
+function verifyFixed(buf){
+    const startIndex=0;
+
+
+    let [result, index, n_buff] = MarkovEngine(startIndex, buf);
+    
+    if(!result){
+	console.log(`The length is ${buf.length}`)
+	console.log("Failed the index is below");
+	console.log(index, `${buf[index]}`);
+	console.log(buf);
+    }
+
+}
+
+//verifyFixed(evilBuffer17);
