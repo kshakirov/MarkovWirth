0387bc9d7ead6e57cb2efb82ef5d339f94f02c98
Kirill Shakirov <kirill.shakirov4@gmail.com>
2026-08-11 13:20:09 +0300

    almost there

--- test/test_wirth_parser.js
+++ test/test_wirth_parser.js
@@ -12,10 +12,11 @@ const csv = `pos,id,name,
 4,400,a,
 5,500,b
 `
-const csv2= `"""i""","""eg60an7o"""
+const csv2= `"""utjfwf""",f5c1za04l,"""68op6""",1
 `
-
-
+const csv3 = `ip3xrpe,"""u7v""","""7xfxbfbqc""","""yh""","""qj5xckcy""",1
+`
+const csv4 = `"""0f2rn""","""xzj""",548`
 let buffer = Buffer.from(csv2);
 
 let result = WirthCsvParser(buffer, 0, buffer.length);
