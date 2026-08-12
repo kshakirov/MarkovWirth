ae599036785185c13df13d76cc0f74a3a5b3ff13
Kirill Shakirov <kirill.shakirov4@gmail.com>
2026-08-12 16:08:36 +0300

    new line after quotes added and fixed

--- test/test_wirth_parser.js
+++ test/test_wirth_parser.js
@@ -4,12 +4,12 @@ import {Buffer} from "node:buffer"
 
 
 
-const csv = `pos,id,name,
-1,here it is 100,a,
-2,200,k,
-3," ""3"" ",1,
-77,"""i""","""eg60an7o""",
-4,400,a,
+const csv = `pos,id,name
+1,here it is 100,a
+2,200,k
+3," ""3"" ",1
+77,"""i""","""eg60an7o"""
+4,400,a
 5,500,b
 `
 const csv2= `"""utjfwf""",f5c1za04l,"""68op6"""
@@ -17,7 +17,7 @@ const csv2= `"""utjfwf""",f5c1za04l,"""68op6"""
 const csv3 = `ip3xrpe,"""u7v""","""7xfxbfbqc""","""yh""","""qj5xckcy""",1
 `
 const csv4 = `"""0f2rn""","""xzj""",548`
-let buffer = Buffer.from(csv2);
+let buffer = Buffer.from(csv);
 
 let result = WirthCsvParser(buffer, 0, buffer.length);
 console.log(result);
