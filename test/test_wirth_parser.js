3d49e6a422b65c8e8387ceb8ce5df053d41d0f4b
Kirill Shakirov <kirill.shakirov4@gmail.com>
2026-08-13 15:40:35 +0300

    before Markove changed

--- test/test_wirth_parser.js
+++ test/test_wirth_parser.js
@@ -1,5 +1,6 @@
 import {MarkovEngine} from "./../lib/parser/markov/parse_escaped_byte_no_window.js"
 import {WirthCsvParser} from "../bin/wirth_csv_parser.js"
+import {WirthCsvParserFlat} from "../bin/wirth_csv_parser_flat.js"
 import {Buffer} from "node:buffer"
 
 
@@ -7,7 +8,7 @@ import {Buffer} from "node:buffer"
 const csv = `pos,id,name
 1,here it is 100,a
 2,200,k
-3," ""3"" ",1
+3,1," ""3"" "
 77,"""i""","""eg60an7o"""
 4,400,a
 5,500,b
@@ -19,5 +20,17 @@ const csv3 = `ip3xrpe,"""u7v""","""7xfxbfbqc""","""yh""","""qj5xckcy""",1
 const csv4 = `"""0f2rn""","""xzj""",548`
 let buffer = Buffer.from(csv);
 
-let result = WirthCsvParser(buffer, 0, buffer.length);
+// let result = WirthCsvParser(buffer, 0, buffer.length);
+// console.log(result);
+
+
+let result = WirthCsvParserFlat(buffer, buffer.length);
 console.log(result);
+
+for(let l of result){
+    for(let ll of l){
+	console.log(`${ll}`);
+
+    }
+    console.log("#################################")
+}
