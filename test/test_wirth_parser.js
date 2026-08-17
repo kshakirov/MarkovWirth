0c58f4198823c19c9ca939b267f85e5849ef0a24
Kirill Shakirov <kirill.shakirov4@gmail.com>
2026-08-17 15:56:06 +0300

    new line found issue for last column

--- test/test_wirth_parser.js
+++ test/test_wirth_parser.js
@@ -9,7 +9,7 @@ const csv = `pos,id,name
 1,here it is 100,a
 2,200,k
 3,1," ""3"" "
-77,"""i""","""eg60an7o"""
+77,"""i""","""eg60an7o here will \n be new line"""
 4,400,a
 5,500,b
 `
@@ -17,13 +17,27 @@ const csv2= `"""utjfwf""",f5c1za04l,"""68op6"""
 `
 const csv3 = `ip3xrpe,"""u7v""","""7xfxbfbqc""","""yh""","""qj5xckcy""",1
 `
-const csv4 = `"""0f2rn""","""xzj""",548`
-let buffer = Buffer.from(csv);
+const csv4 = `"""0f2rn""","""xzj""",548
+`
 
-// let result = WirthCsvParser(buffer, 0, buffer.length);
+
+const csv5 = `"ki3dt8sgd
+a38tww"
+`
+const csv6 = `"ki3dt8sgd
+dd2cx
+p5megc
+hrmg
+z,,wr1x5
+c0d4p,8bty2yh
+8,48jfq 
+c,aiwf
+a38tww"
+`
+//let result = WirthCsvParser(buffer, 0, buffer.length);
 // console.log(result);
 
-
+let buffer = Buffer.from(csv6);
 let result = WirthCsvParserFlat(buffer, buffer.length);
 console.log(result);
 
