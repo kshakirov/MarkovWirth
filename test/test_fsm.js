2c43d5a1a49c1535f516f3b399e74626ea654042
Kirill Shakirov <kirill.shakirov4@gmail.com>
2026-08-20 14:08:47 +0300

    REFACTORING 5

--- test/test_fsm.js
+++ test/test_fsm.js
@@ -3,8 +3,18 @@ import {Buffer} from "node:buffer"
 
 const csv = `pos,id,name
 `;
-const itable = new Int16Array(256);
-let buffer = Buffer.from(csv);
-let result = WirthMarkovFSM(buffer, buffer.length,itable );
+const STATE_CELL = 2;       // Обычное чтение ячейки
+const STATE_QUOTED = 1;     // Внутри кавычек
+const STATE_QUOTE_CHECK = 3;// Проверка экранирования (встретили кавычку внутри кавычек)
+const STATE_START = 0;
+
+// Флаги-сентинелы для ленты разметки
+const ROW_END_SENTINEL = -1
+const itable = new Int32Array(256);
+let buffer = Buffer.from(csv),
+    itable_pointer= 0,
+    state= STATE_START; //celle
+
+let result = WirthMarkovFSM(buffer, buffer.length,itable, itable_pointer, state );
 console.log(result);
 
