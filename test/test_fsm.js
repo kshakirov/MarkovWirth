4d9b3cd88ecf70419741ca057b792c0a2f8f492c
Kirill Shakirov <kirill.shakirov4@gmail.com>
2026-08-20 13:52:48 +0300

    REFACTORING 4

--- test/test_fsm.js
+++ test/test_fsm.js
@@ -3,8 +3,8 @@ import {Buffer} from "node:buffer"
 
 const csv = `pos,id,name
 `;
-
+const itable = new Int16Array(256);
 let buffer = Buffer.from(csv);
-let result = WirthMarkovFSM(buffer, buffer.length);
+let result = WirthMarkovFSM(buffer, buffer.length,itable );
 console.log(result);
 
